e3aa19fd3c56bb75f399f647d4fadf743aef7cf7
Stevoni <tanner65@gmail.com>
2016-09-27 00:07:28 -0500

    Initial Commit

---
+++ src/CopyFileUtility/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CopyFileUtility
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length == 1)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new frmMain(new FileInfo(args[0])));
+            }
+        }
+    }
+}
