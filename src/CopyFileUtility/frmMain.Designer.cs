50a7ae75eef117916ca7abed67edabcd737e48a8
Stevoni <tanner65@gmail.com>
2016-09-27 00:07:28 -0500

    Initial Code Commit

---
+++ src/CopyFileUtility/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CopyFileUtility
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvPaths = new System.Windows.Forms.ListView();
+            this.colOutputType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colOutput = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvPaths
+            // 
+            this.lvPaths.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lvPaths.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colOutputType,
+            this.colOutput});
+            this.lvPaths.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvPaths.FullRowSelect = true;
+            this.lvPaths.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvPaths.HoverSelection = true;
+            this.lvPaths.Location = new System.Drawing.Point(0, 0);
+            this.lvPaths.Name = "lvPaths";
+            this.lvPaths.Size = new System.Drawing.Size(459, 117);
+            this.lvPaths.TabIndex = 0;
+            this.lvPaths.UseCompatibleStateImageBehavior = false;
+            this.lvPaths.View = System.Windows.Forms.View.Details;
+            this.lvPaths.ItemActivate += new System.EventHandler(this.lvPaths_ItemActivate);
+            // 
+            // colOutputType
+            // 
+            this.colOutputType.Text = "Output Type";
+            // 
+            // colOutput
+            // 
+            this.colOutput.Text = "Output";
+            this.colOutput.Width = 120;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 117);
+            this.Controls.Add(this.lvPaths);
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "Copy File Utility";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvPaths;
+        private System.Windows.Forms.ColumnHeader colOutputType;
+        private System.Windows.Forms.ColumnHeader colOutput;
+    }
+}
+
---
+++ src/CopyFileUtility/frmMain.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace CopyFileUtility
+{
+    public partial class frmMain : Form
+    {
+
+        private System.IO.FileInfo currentFile;
+
+        public frmMain(System.IO.FileInfo file)
+        {
+            InitializeComponent();
+
+            currentFile = file;
+
+            PopulateListBox();
+            UpdateColumnWidths();
+        }
+
+        private void CheckFileExistsOrClose()
+        {
+            if (currentFile.Exists == false)
+            {
+                Environment.Exit(-1);
+            }
+        }
+
+        private void UpdateColumnWidths()
+        {
+            lvPaths.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            colOutput.Width = -2;
+        }
+        private void PopulateListBox()
+        {
+            foreach (KeyValuePair<PathMappingAttribute, MethodInfo> pair in GetAttributes())
+            {
+                string output = pair.Value.Invoke(this, null) as string;
+
+                ListViewItem item = new ListViewItem(new string[] { pair.Key.OutputType, output });
+                item.Tag = output;
+
+                lvPaths.Items.Add(item);
+            }
+        }
+
+        private List<KeyValuePair<PathMappingAttribute, MethodInfo>> GetAttributes()
+        {
+            List<KeyValuePair<PathMappingAttribute, MethodInfo>> result = new List<KeyValuePair<PathMappingAttribute, MethodInfo>>();
+            MethodInfo[] methods = GetType().GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (MethodInfo method in methods)
+            {
+                PathMappingAttribute[] attributes = method.GetCustomAttributes(typeof(PathMappingAttribute), true) as PathMappingAttribute[];
+
+                if (attributes != null)
+                {
+                    foreach (PathMappingAttribute attribute in attributes)
+                    {
+                        result.Add(new KeyValuePair<PathMappingAttribute, MethodInfo>(attribute, method));
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        #region File resolution
+
+        [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+        private class PathMappingAttribute : Attribute
+        {
+            public PathMappingAttribute(string outputType)
+            {
+                OutputType = outputType;
+            }
+
+            public string OutputType { get; private set; }
+        }
+
+        [PathMapping("Full Path")]
+        private string GetFullPath()
+        {
+            return currentFile.FullName;
+        }
+
+        [PathMapping("File Name")]
+        private string GetFileName()
+        {
+            return currentFile.Name;
+        }
+
+        [PathMapping("Short Path")]
+        private string GetShortPath()
+        {
+            const int MAX_PATH = 255;
+            StringBuilder result = new StringBuilder(MAX_PATH);
+
+            if (GetShortPathName(currentFile.FullName, result, MAX_PATH) > 0)
+            {
+                return result.ToString();
+            }
+            else
+            {
+                return currentFile.FullName;
+            }
+        }
+
+        [PathMapping("URL")]
+        private string GetURL()
+        {
+            return new Uri(currentFile.FullName).ToString();
+        }
+
+        [PathMapping("UNC")]
+        private string GetUNC()
+        {
+            return string.Empty;
+        }
+
+        [PathMapping("UNC URL")]
+        private string GetUNCURL()
+        {
+            return string.Empty;
+        }
+
+        #region Imports
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName([MarshalAs(UnmanagedType.LPTStr)]string path, [MarshalAs(UnmanagedType.LPTStr)]StringBuilder shortPath, int shortPathLength);
+        #endregion
+
+        #endregion
+
+        private void lvPaths_ItemActivate(object sender, EventArgs e)
+        {
+            CopyCurrentItemToClipboard();
+        }
+
+        private void CopyCurrentItemToClipboard()
+        {
+            if (lvPaths.SelectedItems.Count == 1)
+            {
+                Hide();
+
+                string output = lvPaths.SelectedItems[0].Tag as string;
+                if (string.IsNullOrEmpty(output) == false)
+                {
+                    Clipboard.SetText(output);
+                }
+                
+                Close();
+            }
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CopyCurrentItemToClipboard();
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if (lvPaths.Items.Count > 0)
+            {
+                lvPaths.Items[0].Selected = true;
+            }
+        }
+    }
+}
